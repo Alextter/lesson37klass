0a4b79ee809d7ed79a3b0fd4633854493ed471dc
Alextter <74066029+Alextter@users.noreply.github.com>
2021-03-15 19:49:40 +0200

    Add files via upload

---
+++ lesson37/main.js
@@ -0,0 +1,31 @@
+// arr = ["Ponedelnik", "Vtornik", "Sreda", "Chetverg",  "Patnica", "Subbota", "Voskres"]
+// alert(arr[0] + " i " + arr[4] + " Ekzamen")
+
+
+// arr = ["Jenia", "Sasha", "Misha", "Dana", "Vova","poluchit", "podarok", "ne"]
+
+// alert(arr[0] + " i " + arr[3] + " i " + arr[4] + " i " + arr[5] +  " i " + arr[5] + " i " + arr[6])
+// alert(arr[1] + " i " + arr[2] + " "  + arr[7] + " " + arr[5] + " " + arr[6] ) 
+
+// obj = {"Jena" : "2000" , "Dana" : "45000" , "Bogdan" : "30000"}
+// alert(obj["Bogdan"])
+
+//  arr = {
+//      "Jena":["109000" , "15000" ,  "20000"] ,
+//      "Sasha":["10900000" , "1500000" , "2000000"] 
+//  }
+//  alert(arr["Sasha"][1])
+// arr = {
+//     "Malchik" : [ "Sasha " , "Tataru" , "Den" , "Misha" , "Vlad"],
+//     "Devichki" : ["Dana", "Nella" , "Arina" , "Masha"]
+// }
+// alert(arr["Malchik"][0] + " " +  arr["Devichki"][0] + " " + arr["Malchik"][1])
+// let a = 1
+// arr  = []
+// arr [0] = 2
+// arr [1] = 3
+// arr [2] = 4
+// arr [3] = 5
+// for ( i=0 ; i<4 ;i++)
+// { a=arr[i] * a}
+// alert(a)
